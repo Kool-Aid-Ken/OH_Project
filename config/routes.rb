9258294f2d98b66dc17022131f5bb2ebc9c72f84
Kenneth Carthen Jr <kenneth.carthen@hgs.hiddengeniusproject.org>
2019-10-31 16:49:21 -0700

    Pushed OH Project to Git

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :deals
+  get "/deals_1", to: "deals#deals_1"
+  get "/deals_2", to: "deals#deals_2"
+  get "/deals_3", to: "deals#deals_3"
+
 end
